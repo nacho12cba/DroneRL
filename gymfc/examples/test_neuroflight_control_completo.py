e22d47c87f78fd423837ebfe0a3a87a0f601dd34
yannekola <yan.nekola@gmail.com>
2021-04-29 19:41:23 -0300

    +tests test_neuroflight_control_completo

--- gymfc/examples/test_neuroflight_control_completo.py
+++ gymfc/examples/test_neuroflight_control_completo.py
@@ -57,7 +57,7 @@ def flight(checkpoint_path,env,num_trials=1,inputs=[[0,0,0]]):  ##una función, 
                 
                 
                 if env.sim_time > 10000: ob, reward, done,  _ = env.step(ac) ##entiendo que asigna a las variables ob, reward, done, _, algo que retorna el step del entorno cuando toma como argumento la acción
-                else env.step(0.4,0.4,0.4,0.4)                                   ##necesito buscar la función step. En base.py hay una función step, así que supongo que env siempre va a heredar de base.py. 
+                else: ob, reward, done,  _ = env.step(0.4,0.4,0.4,0.4)                                   ##necesito buscar la función step. En base.py hay una función step, así que supongo que env siempre va a heredar de base.py. 
                                                     ##la step de base.py devuelve: return state, reward, done, {}. No sé qué es llaves, pero bueno. Algo nulo, pero no sé por qué se correspondería con el _
                                                     ##además acá ejecuta el paso de simulación, que en teoría devuelve la información de estado resultante
                                                     ##así que en el step debería consultarse la información de "medición" que necesito
